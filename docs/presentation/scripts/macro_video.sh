4b88f28d37455a59408477a08b6c24e67f8ae84b
Victor Santos <viic.santos@gmail.com>
2018-12-15 23:38:57 -0200

    Added documents and project diagrams

---
+++ docs/presentation/scripts/macro_video.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#file:///home/victor/workspace/tcc/docs/coletor_bolas_tenis/apresentacao/scripts/macro_video.sh
+xine --no-splash --fullscreen /home/victor/workspace/tcc/docs/coletor_bolas_tenis/apresentacao/video/testeFinal_semAudio.avi
+
